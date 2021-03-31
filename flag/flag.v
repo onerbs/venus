b30982e92307a4933a364c2c666a08f4b77c11be
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-03-31 03:48:22 -0600

    flag: Read numeric data types

--- flag/flag.v
+++ flag/flag.v
@@ -1,5 +1,7 @@
 module flag
 
+import str
+
 // bool check if a flag is present in the argument list
 pub fn bool(mut args []string, flags []string) bool {
 	mut status := false
@@ -21,9 +23,51 @@ pub fn value(mut args []string, flags []string) ?string {
 				args.delete(index)
 				if index < args.len {
 					res := args[index]
-					if res.len > 0 && res[0] != `-` {
+					if res.len > 0 && res[0] != `-` { // !
 						args.delete(index)
-						return res
+						return res // !
+					}
+				}
+				break
+			}
+		}
+	}
+	return none
+}
+
+// todo: copy-paste ...
+
+// float get a float value from the argument list
+pub fn float(mut args []string, flags []string) ?f64 {
+	for flag in flags {
+		for index in 0 .. args.len {
+			if args[index] == flag {
+				args.delete(index)
+				if index < args.len {
+					res := args[index]
+					if str.is_numeric(res) { // !
+						args.delete(index)
+						return res.f64() // !
+					}
+				}
+				break
+			}
+		}
+	}
+	return none
+}
+
+// int get an integer value from the argument list
+pub fn int(mut args []string, flags []string) ?int {
+	for flag in flags {
+		for index in 0 .. args.len {
+			if args[index] == flag {
+				args.delete(index)
+				if index < args.len {
+					res := args[index]
+					if str.is_integer(res) { // !
+						args.delete(index)
+						return res.int() // !
 					}
 				}
 				break
--- flag/flag_test.v
+++ flag/flag_test.v
@@ -11,19 +11,37 @@ fn test_bool() {
 }
 
 fn test_value() {
-	mut args := ['some', '--dir', 'ahoy', 'argument', '-f']
-	dir := flag.value(mut args, ['-d', '--dir']) or { 'some_dir' }
-	file := flag.value(mut args, ['-f', '--file']) or { 'some_file' }
-	assert dir == 'ahoy'
-	assert file == 'some_file'
+	mut args := ['some', '-d', 'ahoy', 'argument', '-f']
+	f_d := flag.value(mut args, ['-d']) or { 'some_d' }
+	f_f := flag.value(mut args, ['-f']) or { 'some_f' }
+	assert f_d == 'ahoy'
+	assert f_f == 'some_f'
 	assert args == ['some', 'argument']
 }
 
 fn test_value_two() {
 	mut args := ['some', '-df', 'argument']
-	dir := flag.value(mut args, ['-d', '--dir']) or { 'some_dir' }
-	file := flag.value(mut args, ['-f', '--file']) or { 'some_file' }
-	assert dir == 'some_dir'
-	assert file == 'some_file'
+	f_d := flag.value(mut args, ['-d']) or { 'some_d' }
+	f_f := flag.value(mut args, ['-f']) or { 'some_f' }
+	assert f_d == 'some_d'
+	assert f_f == 'some_f'
 	assert args == ['some', '-df', 'argument']
+}
+
+fn test_float() {
+	mut args := ['some', '-w', '0.88', 'plus']
+	f_w := flag.float(mut args, ['-w']) or { 45.3 }
+	f_z := flag.float(mut args, ['-z']) or { 66 }
+	assert f_w == 0.88
+	assert f_z == 66.0
+	assert args == ['some', 'plus']
+}
+
+fn test_float_two() {
+	mut args := ['some', '-w', 'plus']
+	f_w := flag.float(mut args, ['-w']) or { 45.3 }
+	f_z := flag.float(mut args, ['-z']) or { 66 }
+	assert f_w == 45.3
+	assert f_z == 66.0
+	assert args == ['some', 'plus']
 }

1c32704feb5255b9055be4df516424c911ca18fe
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-04-02 19:19:31 -0600

    xsh: :memo: Add documentation

--- xsh.c.v
+++ xsh.c.v
@@ -2,6 +2,7 @@ module xsh
 
 import os
 
+// get_input read from the standard input but allow empty lines
 pub fn get_input() ?string {
 	mut buff := []byte{cap: 0x400}
 	for {
@@ -17,6 +18,7 @@ pub fn get_input() ?string {
 	return none
 }
 
+// get_lines read all lines from the standard input but allow empty lines
 pub fn get_lines() ?[]string {
 	mut lines := []string{}
 	mut buff := []byte{cap: 0x100}
@@ -43,6 +45,7 @@ pub fn get_lines() ?[]string {
 
 fn C.fgetc(&C.FILE) int
 
+// read_file read the content of the given text file
 pub fn read_file(file string) ?string {
 	fp := os.vfopen(file, 'r') ?
 	mut buff := []byte{cap: 0x400}
@@ -60,6 +63,7 @@ pub fn read_file(file string) ?string {
 	return none
 }
 
+// read_file read the lines of the given text file
 pub fn read_lines(file string) ?[]string {
 	fp := os.vfopen(file, 'r') ?
 	mut buff := []byte{cap: 0x100}

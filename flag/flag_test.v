97effca42bbc2986ff8b8ca6f1af181f06c5d410
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-04-29 12:33:57 -0500

    flag!: Update tests

--- flag/flag_test.v
+++ flag/flag_test.v
@@ -2,27 +2,27 @@ module main
 
 import xsh.flag
 
-fn test_bool() {
+fn test_got() {
 	mut args := ['some', '--dir', 'argument', '-f']
-	assert flag.bool(mut args, ['-d', '--dir'])
-	assert flag.bool(mut args, ['-f', '--file'])
-	assert !flag.bool(mut args, ['-h', '--help'])
+	assert flag.got(mut args, '-d', '--dir')
+	assert flag.got(mut args, '-f', '--file')
+	assert !flag.got(mut args, '-h', '--help')
 	assert args == ['some', 'argument']
 }
 
-fn test_value() {
+fn test_get() {
 	mut args := ['some', '-d', 'ahoy', 'argument', '-f']
-	f_d := flag.value(mut args, ['-d']) or { 'some_d' }
-	f_f := flag.value(mut args, ['-f']) or { 'some_f' }
+	f_d := flag.get(mut args, '-d') or { 'some_d' }
+	f_f := flag.get(mut args, '-f') or { 'some_f' }
 	assert f_d == 'ahoy'
 	assert f_f == 'some_f'
 	assert args == ['some', 'argument']
 }
 
-fn test_value_two() {
+fn test_get_two() {
 	mut args := ['some', '-df', 'argument']
-	f_d := flag.value(mut args, ['-d']) or { 'some_d' }
-	f_f := flag.value(mut args, ['-f']) or { 'some_f' }
+	f_d := flag.get(mut args, '-d') or { 'some_d' }
+	f_f := flag.get(mut args, '-f') or { 'some_f' }
 	assert f_d == 'some_d'
 	assert f_f == 'some_f'
 	assert args == ['some', '-df', 'argument']
@@ -30,8 +30,8 @@ fn test_value_two() {
 
 fn test_float() {
 	mut args := ['some', '-w', '0.88', 'plus']
-	f_w := flag.float(mut args, ['-w']) or { 45.3 }
-	f_z := flag.float(mut args, ['-z']) or { 66 }
+	f_w := flag.float(mut args, '-w') or { 45.3 }
+	f_z := flag.float(mut args, '-z') or { 66 }
 	assert f_w == 0.88
 	assert f_z == 66.0
 	assert args == ['some', 'plus']
@@ -39,8 +39,8 @@ fn test_float() {
 
 fn test_float_two() {
 	mut args := ['some', '-w', 'plus']
-	f_w := flag.float(mut args, ['-w']) or { 45.3 }
-	f_z := flag.float(mut args, ['-z']) or { 66 }
+	f_w := flag.float(mut args, '-w') or { 45.3 }
+	f_z := flag.float(mut args, '-z') or { 66 }
 	assert f_w == 45.3
 	assert f_z == 66.0
 	assert args == ['some', 'plus']

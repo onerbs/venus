426a347a034bd1486ea2273d139f579909966a3a
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-04-29 12:32:51 -0500

    flag!: Minor improvements  - Rename `bool->got` and `value->get`

--- flag/flag.v
+++ flag/flag.v
@@ -2,31 +2,58 @@ module flag
 
 import str
 
-// bool check if a flag is present in the argument list
-pub fn bool(mut args []string, flags []string) bool {
-	mut status := false
-	for flag in flags {
-		index := args.index(flag)
-		if index >= 0 {
-			args.delete(index)
-			status = true
+// got check if a flag is present in the args
+pub fn got(mut args []string, flags ...string) bool {
+	for it in flags {
+		ix := args.index(it)
+		if ix >= 0 {
+			args.delete(ix)
+			return true
 		}
 	}
-	return status
+	return false
 }
 
-// value get a flagged value from the argument list
-pub fn value(mut args []string, flags []string) ?string {
-	for flag in flags {
-		for index in 0 .. args.len {
-			if args[index] == flag {
-				args.delete(index)
-				if index < args.len {
-					res := args[index]
-					if res.len > 0 && res[0] != `-` { // !
-						args.delete(index)
-						return res // !
-					}
+// Q: parse flags in the form `--some=soma` ?
+
+// get returns a value from the argument list
+pub fn get(mut args []string, flags ...string) ?string {
+	ix, res := get_index(mut args, flags) ?
+	if !is_flag(res) {
+		args.delete(ix)
+		return res
+	}
+	return none
+}
+
+// float returns a float value from the argument list
+pub fn float(mut args []string, flags ...string) ?f64 {
+	ix, res := get_index(mut args, flags) ?
+	if str.is_numeric(res) {
+		args.delete(ix)
+		return res.f64()
+	}
+	return none
+}
+
+// int returns an integer value from the argument list
+pub fn int(mut args []string, flags ...string) ?int {
+	ix, res := get_index(mut args, flags) ?
+	if str.is_integer(res) {
+		args.delete(ix)
+		return res.int()
+	}
+	return none
+}
+
+// get_index returns the index of the flag value
+fn get_index(mut args []string, flags []string) ?(int, string) {
+	for it in flags {
+		for ix in 0 .. args.len {
+			if args[ix] == it {
+				args.delete(ix)
+				if ix < args.len {
+					return ix, args[ix]
 				}
 				break
 			}
@@ -35,44 +62,11 @@ pub fn value(mut args []string, flags []string) ?string {
 	return none
 }
 
-// todo: copy-paste ...
-
-// float get a float value from the argument list
-pub fn float(mut args []string, flags []string) ?f64 {
-	for flag in flags {
-		for index in 0 .. args.len {
-			if args[index] == flag {
-				args.delete(index)
-				if index < args.len {
-					res := args[index]
-					if str.is_numeric(res) { // !
-						args.delete(index)
-						return res.f64() // !
-					}
-				}
-				break
-			}
-		}
+// is_flag check if the provided value is a flag or not
+fn is_flag(raw string) bool {
+	return match raw.len {
+		0 { false }
+		1 { raw == '-' }
+		else { raw[0] == `-` && !raw[1].is_digit() }
 	}
-	return none
-}
-
-// int get an integer value from the argument list
-pub fn int(mut args []string, flags []string) ?int {
-	for flag in flags {
-		for index in 0 .. args.len {
-			if args[index] == flag {
-				args.delete(index)
-				if index < args.len {
-					res := args[index]
-					if str.is_integer(res) { // !
-						args.delete(index)
-						return res.int() // !
-					}
-				}
-				break
-			}
-		}
-	}
-	return none
 }
